f27247dde2e2d8f6952c61289512b89eac1c7b61
youkiery <youkiery@gmail.com>
2021-10-24 21:44:16 +0700

    giá sỉ

---
+++ server/price.php
@@ -0,0 +1,71 @@
+<?php
+function init() {
+  global $data, $db, $result;
+
+  $result['status'] = 1;
+  $result['list'] = getlist();
+  return $result;
+}
+
+function insert() {
+  global $data, $db, $result;
+ 
+  $sql = "insert into pet_test_price (name) values('$data->name')";
+  $itemid = $db->insertid($sql);
+
+  foreach ($data->detail as $detail) {
+    $sql = "insert into pet_test_price_detail (itemid, name, price) values($itemid, '$detail->name', '$detail->price')";
+    $db->query($sql);
+  }
+ 
+  $result['status'] = 1;
+  $result['list'] = getlist();
+  return $result; 
+}
+
+function remove() {
+  global $data, $db, $result;
+
+  $sql = "delete from pet_test_price where id = $data->id";
+  $db->query($sql);
+  $sql = "delete from pet_test_price_detail where itemid = $data->id";
+  $db->query($sql);
+
+  $result['status'] = 1;
+  $result['list'] = getlist();
+  return $result; 
+}
+
+function update() {
+  global $data, $db, $result;
+
+  $sql = "delete from pet_test_price_detail where itemid = $data->id";
+  $db->query($sql);
+
+  $sql = "update pet_test_price set name = '$data->name' where id = $data->id";
+  $db->query($sql);
+
+  foreach ($data->detail as $detail) {
+    $sql = "insert into pet_test_price_detail (itemid, name, price) values($data->id, '$detail->name', '$detail->price')";
+    $db->query($sql);
+  }
+  
+  $result['status'] = 1;
+  $result['list'] = getlist();
+  return $result; 
+}
+
+function getlist() {
+  global $db, $data;
+
+  $xtra = "";
+  if (isset($data->{'keyword'})) $xtra = "where name like '%$data->keyword%'";
+  $sql = "select * from pet_test_price $xtra order by id desc";
+  $pl = $db->all($sql);
+
+  foreach ($pl as $i => $p) {
+    $sql = "select * from pet_test_price_detail where itemid = '$p[id]' order by id asc";
+    $pl[$i]['detail'] = $db->all($sql);
+  }
+  return $pl;
+}
